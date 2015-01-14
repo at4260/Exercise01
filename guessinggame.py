a428d1854cecb5902f1edc317a2a204f030b8527
Hackbright Student <info@hackbrightacademy.com>
2015-01-13 17:57:11 -0800

    added counter

--- guessinggame.py
+++ guessinggame.py
@@ -5,6 +5,7 @@ name = raw_input("What's your name?")
 print "Guess a random integer between 1-100!"
 
 correct_number = random.randint(1,101)
+counter = 0
 
 while True:
     g_guess = raw_input("Your guess?")
@@ -19,8 +20,11 @@ while True:
                 print "Your guess is too low, try again."
             else:
                 print "You did it!"
+                counter += 1
                 break
     except ValueError:
         print "Guess a REAL number between 1-100!"
-    
+    counter += 1
+
+print "Well done! You found my number in %s tries!" % counter
         
