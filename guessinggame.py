43b00398a80aaef4f473f5f9d79d8fc95015b87e
Hackbright Student <info@hackbrightacademy.com>
2015-01-13 16:31:24 -0800

    input checks not working

--- guessinggame.py
+++ guessinggame.py
@@ -7,11 +7,18 @@ print "Guess a random number between 1-100!"
 correct_number = random.randint(1,101)
 
 while True:
-    number = input("Your guess?")
-    if number > correct_number:
-        print "Your guess is too high, try again."
-    elif number < correct_number:
-        print "Your guess is too low, try again."
+    g_guess = raw_input("Your guess?")
+    if type(g_guess) != int:
+        print "Guess a REAL number between 1-100!"
     else:
-        print "You did it!"
-        break
+        number = int(raw_input("Your guess?"))
+        if number > 100 or number < 1:
+            print "1 AND 100!!!"
+        else:
+            if number > correct_number:
+                print "Your guess is too high, try again."
+            elif number < correct_number:
+                print "Your guess is too low, try again."
+            else:
+                print "You did it!"
+                break
